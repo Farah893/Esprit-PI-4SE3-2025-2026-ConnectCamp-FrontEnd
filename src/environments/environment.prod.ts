feef71239108480dabc58f5e8da4857e1ae0386f
Farah893 <yaichfarah1@gmail.com>
2026-05-04 22:10:10 +0200

    Update API URL for Azure deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
   /** Empty string = same origin; nginx / ingress reverse-proxies /api, /auth, /uploads to the backend. */
-  apiUrl: '',
+  apiUrl: 'https://campconnect-pi-d9emffg5dba8eqe9.southafricanorth-01.azurewebsites.net',
   allowOfflineAuth: false
 };
